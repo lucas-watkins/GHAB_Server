422433bbe10a2ad5c5877a6e64bd9de7e472dcfa
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2024-04-12 23:13:36 -0400

    Images are now transmittable

--- obj/Debug/net8.0/HAB_WebServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/HAB_WebServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HAB_WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3f1fb07e8a313f70277eaecb3b83f5106a4170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92f5f7d3da2b69a62e36f83f297cbea31395b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("HAB_WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HAB_WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
